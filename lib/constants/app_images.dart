e6ca30cb9644c5f9a5c64dc76ac594bd99df3d36
Muhammad Huzaifa Khan <72612691+muhammadhuzaifakhan1133@users.noreply.github.com>
2025-09-20 20:20:14 +0500

    relocate images

--- lib/constants/app_images.dart
+++ lib/constants/app_images.dart
@@ -1,7 +1,7 @@
 class AppImages {
-  static const String logo = 'assets/logo.png';
-  static const String onboarding1 = 'assets/onboarding1.png';
-  static const String onboarding2 = 'assets/onboarding2.png';
-  static const String onboarding3 = 'assets/onboarding3.png';
-  static const String onboarding4 = 'assets/onboarding4.png';
+  static const String logo = 'assets/images/logo.png';
+  static const String onboarding1 = 'assets/images/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding2.png';
+  static const String onboarding3 = 'assets/images/onboarding3.png';
+  static const String onboarding4 = 'assets/images/onboarding4.png';
 }
